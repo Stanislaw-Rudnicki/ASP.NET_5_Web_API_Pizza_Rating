21f7db1b43abd106474ded12ce0d6fcee9ae8555
Stanisław Rudnicki <68515773+Stanislaw-Rudnicki@users.noreply.github.com>
2022-11-01 22:38:43 +0200

    Add project files.

---
+++ Controllers/PizzaRatingController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using WebApplication9_Pizza_json.Models;
+
+namespace WebApplication9_Pizza_json.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaRatingController : ControllerBase
+    {
+        private readonly ILogger<PizzaRatingController> _logger;
+
+        public PizzaRatingController(ILogger<PizzaRatingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post(List<Pizza> pizzas)
+        {
+            var watch = Stopwatch.StartNew();
+
+            List<Pizza> rating = new();
+            var groups = pizzas.GroupBy(p => p.Name).OrderByDescending(g => g.Count()).Take(10);
+
+            foreach (var group in groups)
+            {
+                rating.Add(new Pizza() { Orders = group.Count(), Toppings = group.FirstOrDefault().Toppings });
+            }
+
+            watch.Stop();
+            _logger.LogInformation(watch.ElapsedMilliseconds.ToString() + " ms");
+
+            return Ok(rating);
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication9_Pizza_json.Models
+{
+    public class Pizza
+    {
+        public int Orders { get; set; }
+        public string Name
+        {
+            get
+            {
+                if (Toppings.Count == 0)
+                {
+                    return "";
+                }
+                else
+                {
+                    string name = "";
+                    Toppings.Sort();
+                    foreach (var topping in Toppings)
+                    {
+                        name += topping.Length < 2 ? topping : topping[..2];
+                    }
+                    return name;
+                }
+            }
+        }
+        
+        public List<string> Toppings { get; set; }
+    }
+}
